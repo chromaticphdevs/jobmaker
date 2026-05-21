f032973de33d7d91e0e289ea1eaf5bc16b925628
Mark Angelo Gonzales <chromaticsoftwares@gmail.com>
2026-05-22 03:14:13 +0800

    add prod level importer

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
     define('DBUSER', 'konduefk_live');
     define('DBPASS', 'DR5mM9=#Ll_L');
 
-    define('LIMIT', 100);
+    define('LIMIT', 1000);
     define('TASK_MAX_LIMIT', 1000);
 
     define('SOURCE_UPLOADS_DIR', '/home/konduefk/public_html/public/uploads');
